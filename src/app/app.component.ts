e831ab72404643b6978c58ad35da7205a22e7b4e
Parth Pandey <Parth.Pandey@infobeans.in>
2020-09-05 17:28:39 +0530

    All Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sugat-app';
+
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+    speed: 3000,
+    autoplay: true
+  };
+
+  ngOnInit() {}
+
+  scroll(scrollTo) {
+    let el = document.getElementById(scrollTo);
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
