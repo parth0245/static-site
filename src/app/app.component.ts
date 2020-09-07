f9f01de425f4b145287df7d155a7240afde3f8d0
Parth Pandey <Parth.Pandey@infobeans.in>
2020-09-07 15:31:22 +0530

    slider animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,145 @@ export class AppComponent implements OnInit {
     dots: false,
     prevArrow: false,
     nextArrow: false,
-    speed: 3000,
+    speed: 500,
     autoplay: true
   };
 
-  ngOnInit() {}
+  manOne = {
+    display: '',
+    text: 'Nǐ hǎo!',
+    speedFront: 500,
+    speedBack: 500
+  };
+
+  manTwo = {
+    display: '',
+    text: 'Hello!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manThree = {
+    display: 'Nama',
+    text: 'Namaste!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manFourth = {
+    display: '',
+    text: 'Привет!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manFifth = {
+    display: '',
+    text: 'Cześć!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manSixth = {
+    display: '',
+    text: 'Sawubona!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manSeventh = {
+    display: '',
+    text: '¡Hola!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manEighth = {
+    display: '',
+    text: 'Hi!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  manNinth = {
+    display: '',
+    text: 'Ciao!',
+    speedFront: 450,
+    speedBack: 350
+  };
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manOne');
+    }, 300);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manTwo');
+    }, 450);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manThree');
+    }, 350);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manFourth');
+    }, 650);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manFifth');
+    }, 950);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manSixth');
+    }, 550);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manSeventh');
+    }, 650);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manEighth');
+    }, 450);
+
+    setTimeout(() => {
+      this.typingCallbackForManOne(this, 'manNinth');
+    }, 350);
+  }
 
   scroll(scrollTo) {
     let el = document.getElementById(scrollTo);
     el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  typingCallbackForManOne(that, whichMan) {
+    let totalLength = that[whichMan].text.length;
+    let currentLength = that[whichMan].display.length;
+
+    if (currentLength < totalLength) {
+      that[whichMan].display += that[whichMan].text[currentLength];
+      setTimeout(
+        () => {
+          that.typingCallbackForManOne(that, whichMan);
+        },
+        that[whichMan].speedFront,
+        that
+      );
+    } else {
+      let interval = setInterval(() => {
+        let str = that[whichMan].display;
+        str = str.substring(0, str.length - 1);
+        that[whichMan].display = str;
+        if (that[whichMan].display == '') {
+          setTimeout(
+            () => {
+              that.typingCallbackForManOne(that, whichMan);
+            },
+            that[whichMan].speedFront,
+            that
+          );
+          clearInterval(interval);
+        }
+      }, that[whichMan].speedBack);
+    }
   }
 }
